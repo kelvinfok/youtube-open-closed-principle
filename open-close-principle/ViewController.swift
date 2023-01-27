c8f934123c39e966a4f052fc9cfd96fc2a7e0028
kelvinfok <punkd1@gmail.com>
2023-01-28 04:22:24 +0800

    Initial Commit

---
+++ open-close-principle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  open-close-principle
+//
+//  Created by Kelvin Fok on 28/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
